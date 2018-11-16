85dd06c4b038708d24bdd5caaf1e4b8a14578833
dev-joeld <joel.dalit@xero.com>
2018-11-16 15:34:55 +1300

    Basic layout

--- src/App.js
+++ src/App.js
@@ -1,28 +1,132 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import 'typeface-roboto';
+import withStyles from 'react-jss';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
+const styles = {
+  '@global body': {
+    backgroundColor: '#EEEEEE',
+    color: '#444'
+  },
+  root: {
+    textAlign: 'left'
+  },
+  list: {
+    listStyle: 'none',
+    margin: '0',
+    padding: '0',
+
+    'li': {
+      margin: '0 0 20px 0'
+    }
+  },
+  button: {
+    margin: '20px',
+    '@media (min-width: 1024px)': {
+      margin: '20px'
+    }
+  }
+};
 
 class App extends Component {
+
+  state = {
+    open: false,
+    liveVideoAvailable: false
+  };
+
+  handleClickOpen = () => {
+    this.setState({ open: true });
+  };
+
+  handleSubmit = () => {
+    this.setState({
+      open: false,
+      liveVideoAvailable: true
+    })
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
   render() {
+    const { open } = this.state;
+    const { classes } = this.props;
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className={classes.root}>
+        <ul className={classes.list}>
+          <li>
+            <Button
+              variant="contained"
+              size="large"
+              color="secondary"
+              className={classes.button}
+              onClick={this.handleClickOpen}>
+              One
+            </Button>
+            <Button
+              variant="contained"
+              size="large"
+              color="secondary"
+              className={classes.button}
+              onClick={this.handleClickOpen}>
+              One
+            </Button>
+          </li>
+          <li>
+            <Button variant="contained" size="large" color="primary" className={classes.button}>
+              Two
+            </Button>
+          </li>
+          <li>
+            <Button variant="contained" size="large" color="primary" className={classes.button}>
+              Three
+            </Button>
+          </li>
+        </ul>
+
+        <Dialog open={open} onClose={this.handleClose}>
+          <DialogTitle>Dialog</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              This is the dialog
+            </DialogContentText>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Full name"
+              type="text"
+              fullWidth
+              />
+            <TextField
+              margin="dense"
+              id="viewers"
+              label="Number of people watching"
+              type="tel"
+              fullWidth
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary">
+              Cancel
+            </Button>
+            <Button onClick={this.handleClose} color="primary">
+              Submit
+            </Button>
+          </DialogActions>
+        </Dialog>
       </div>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
